b2739c0514f277bb14cc6df73549615af992c473
Jordan Howlett <jordan@howlett.io>
2014-03-11 22:39:12 -0600

    going to make a branch to try adding wavtap....

--- Cutter/MainViewAppDelegate.h
+++ Cutter/MainViewAppDelegate.h
@@ -8,8 +8,40 @@
 
 #import <Cocoa/Cocoa.h>
 
-@interface MainViewAppDelegate : NSObject <NSApplicationDelegate>
+@interface MainViewAppDelegate : NSObject <NSApplicationDelegate> {
+    NSWindow *window;
+    IBOutlet NSMenu *statusMenu;
+    NSStatusItem *statusItem;
+    
+    NSArray *audioOptionsArray;
+}
+
+// Menu properties
+
+@property (weak) IBOutlet NSMenuItem *quitAppMenuButton;
+@property (weak) IBOutlet NSMenuItem *audioSpeaker;
+@property (weak) IBOutlet NSMenuItem *audioMicrophone;
+@property (weak) IBOutlet NSMenuItem *audioNone;
+
+@property (weak) IBOutlet NSMenuItem *videoFullScreen;
+@property (weak) IBOutlet NSMenuItem *videoScreenSelection;
+@property (weak) IBOutlet NSMenuItem *videoWebcam;
+@property (weak) IBOutlet NSMenuItem *videoNone;
+
+@property (weak) IBOutlet NSMenuItem *selectScreen;
+@property (weak) IBOutlet NSMenuItem *recordToggle;
+
+
+// Menu button actions
+- (IBAction)quitApp:(id)sender;
+- (IBAction)screenSelection:(id)sender;
+
+// Audio actions
+- (IBAction)audioSpeakerSelected:(id)sender;
+- (IBAction)audioMicrophoneSelected:(id)sender;
+- (IBAction)audioNoneSelected:(id)sender;
+
+
 
 @property (assign) IBOutlet NSWindow *window;
-
 @end
--- Cutter/MainViewAppDelegate.m
+++ Cutter/MainViewAppDelegate.m
@@ -13,6 +13,49 @@
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification
 {
     // Insert code here to initialize your application
+    audioOptionsArray = [[NSArray alloc] initWithObjects:self.audioSpeaker,
+                                  self.audioMicrophone,
+                                  self.audioNone, nil];
+    
 }
 
+-(void)awakeFromNib {
+    NSLog(@"HUHUH");
+    statusItem = [[NSStatusBar systemStatusBar] statusItemWithLength:NSVariableStatusItemLength];
+    [statusItem setMenu:statusMenu];
+    [statusItem setTitle:@"Clipper"];
+    [statusItem setHighlightMode:YES];
+}
+
+- (void) quitApp {
+    NSLog(@"quit");
+}
+
+- (IBAction)quitApp:(id)sender {
+       [NSApp terminate: nil];
+}
+
+- (IBAction)screenSelection:(id)sender {
+}
+
+- (IBAction)audioSpeakerSelected:(id)sender {
+    [self resetAudioOptionMenusToStateOff];
+    [sender setState: NSOnState];
+}
+
+- (IBAction)audioMicrophoneSelected:(id)sender {
+    [self resetAudioOptionMenusToStateOff];
+    [sender setState: NSOnState];
+}
+
+- (IBAction)audioNoneSelected:(id)sender {
+    [self resetAudioOptionMenusToStateOff];
+    [sender setState: NSOnState];
+}
+
+- (void)resetAudioOptionMenusToStateOff {
+    for (id object in audioOptionsArray) {
+        [object setState:NSOffState];
+    }
+}
 @end
